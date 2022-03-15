0eecdbb1c12054e4e1e18077b20b0774a224fb34
Gustavo Zanotto <33432435+gutozntt@users.noreply.github.com>
2022-03-14 22:18:16 -0300

    Update check_changes.sh

--- bin/check_changes.sh
+++ bin/check_changes.sh
@@ -5,14 +5,12 @@ ENV=$1
 REGION=$2
 
 if [[ "$BRANCH_NAME" == "main" ]] ; then
-  # shellcheck disable=SC1083
   DIFF=$(git diff --name-only --diff-filter=d "$PREV_SHA" "$SHA")
 else
   DIFF=$(git diff --name-only --diff-filter=d origin/main HEAD)
 fi
 
 echo -e "Git diff :\n${DIFF}"
-# shellcheck disable=SC2196
 if ! echo "${DIFF}" | egrep -q "^infrastructure/${ENV}/${REGION}"; then
   export CHANGED=false
 else
